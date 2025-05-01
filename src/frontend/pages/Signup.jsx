1e4fcdf3ba6d3ca6262ccb3b9376a2641dc5ef20
Alejandro <alevalencigiraldo@gmail.com>
2025-05-01 23:15:06 +0200

    Primer registro y login funcional

--- src/frontend/pages/Signup.jsx
+++ src/frontend/pages/Signup.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { SignUpFormComponent } from '../components/SingUpFormComponent.jsx';
 
 
 export const Signup = () => {
     return (
         <div className="h-screen text-center my-5">
             <h1>Signup!</h1>
+            <SignUpFormComponent />
         </div>
     )
 }
