1d8d3c315ab4dbbe339d110bbd377e7f0cbf1a1a
Alejandro <alevalencigiraldo@gmail.com>
2025-05-03 02:11:46 +0200

    Refactorizacion App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import './frontend/styles/App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Navbar } from './frontend/components/Navbar.jsx'
-import { Home } from './frontend/pages/Home.jsx'
-import { About } from './frontend/pages/About.jsx'
-import { Login } from './frontend/pages/Login.jsx'
 import { Footer } from './frontend/components/Footer.jsx'
-import { Signup } from './frontend/pages/Signup.jsx'
+import { AppRoutes } from './frontend/routes/AppRoutes.jsx'
 
 
 function App() {
@@ -13,13 +10,7 @@ function App() {
     return (
         <BrowserRouter>
             <Navbar />
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/home' element={<Home />}></Route>
-                <Route path='/about' element={<About />}></Route>
-                <Route path='/login' element={<Login />}></Route>
-                <Route path='/signup' element={<Signup />}></Route>
-            </Routes>
+            <AppRoutes />
             <Footer />
         </BrowserRouter>
     )
